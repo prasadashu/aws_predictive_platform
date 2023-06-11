f57c46ac3bdf43320f20cded296e1b24a7ba3831
Ashu Prasad <aprasa29@uic.edu>
2023-06-11 14:08:54 -0500

    Added function to list S3 Buckets

--- app/util/aws-util.ts
+++ app/util/aws-util.ts
@@ -1,49 +1,42 @@
-import * as AWS from "aws-sdk";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 
 /**
- * AWS Utility class to unzip and read CSV files
+ * AWS Utility class to query S3
  */
-export class AWSUtility {
-    /**
-     * Define constructor to authenticate to AWS
-     * @param awsAccessKeyId AWS Access Key
-     * @param awsSecretAccessKey AWS Secret Key
-     */
-    constructor(awsAccessKeyID: string, awsSecretAccessKey: string) {
-        // Authenticate to AWS
-        AWS.config.update(
-            {
-                accessKeyId: awsAccessKeyID,
+export class AWSUtility{
+    // Declare client config variable
+    private client: S3Client;
+
+    // Define the constructor
+    constructor(awsAccessKeyId: string, awsSecretAccessKey: string){
+        // Define S3 client configuration
+        this.client = new S3Client({
+            endpoint: "http://0.0.0.0:4566",
+            credentials: {
+                accessKeyId: awsAccessKeyId,
                 secretAccessKey: awsSecretAccessKey
             }
-        )
-    };
-
-    /**
-     * Define function to get objects from S3
-     * @param bucketName S3 Bucket Name
-     * @param bucketKey S3 Bucket Key
-     * @param endpoint AWS S3 Endpoint
-     */
-    async getS3Object(bucketName: string, bucketKey: string, endpoint: string): Promise<void> {
-        // Instantiate the S3 object
-        let s3 = new AWS.S3({
-            endpoint: endpoint
         });
-        // Get object from S3 bucket
-        s3.getObject(
-            // Define Bucket and Key to fetch data from
-            { Bucket: bucketName, Key: bucketKey }, (error: any, data: any) => {
-                // Check if we have any errors while pulling data
-                if(error) {
-                    // Print error on console
-                    console.log("Error while pulling data from S3: ", error);
-                }
-                else {
-                    // Print the data pulled from S3
-                    console.log("Data pulled from S3: ", data.Body.toString());
-                }
-            }
-        )
     }
+
+    // Define async function to list S3 buckets
+    async listBuckets(): Promise<void> {
+        // Declare input for command to be sent to S3 Server
+        const input = {};
+
+        // Declare command to be sent to S3 Server
+        const command = new ListBucketsCommand(input);
+        
+        // Execute the command
+        try {
+            // Get response from S3 Server
+            const response = await this.client.send(command);
+            // Print the response received
+            console.log(response);
+        } 
+        catch (error) {
+            // Print error while getting response
+            console.error("Error: ", error);
+        }
+    };
 }
