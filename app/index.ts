100f54f8b042e86f26dbc4c5b222630e6326686e
Ashu Prasad <aprasa29@uic.edu>
2023-06-11 17:31:38 -0500

    Called method to list S3 objects

--- app/index.ts
+++ app/index.ts
@@ -3,5 +3,7 @@ import { AWSUtility } from "./util/aws-util";
 // Create the AWSUtility object
 const awsUtility = new AWSUtility("test_access_key_id", "test_secret_access_key_id");
 
-// Call the getS3Bucket method
+// Call the listBuckets method
 awsUtility.listBuckets();
+// Call the listObjects method
+awsUtility.listObjects(process.argv[2]);
