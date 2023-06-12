0c052772f116ed8abf86d1bb94470db7aa3e316c
Ashu Prasad <aprasa29@uic.edu>
2023-06-11 22:23:44 -0500

    Added code to get S3 object

--- app/index.ts
+++ app/index.ts
@@ -7,3 +7,5 @@ const awsUtility = new AWSUtility("test_access_key_id", "test_secret_access_key_
 awsUtility.listBuckets();
 // Call the listObjects method
 awsUtility.listObjects(process.argv[2]);
+// Call the getObject method
+awsUtility.getObject(process.argv[2], process.argv[3]);
