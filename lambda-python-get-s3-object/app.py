2d1fe5201ecec858143396024f6883061dfae01a
Ashu Prasad <ashuprasad29516@gmail.com>
2023-07-17 13:52:28 -0500

    feat: added functionality to receive task and User ID from events through SQS

--- lambda-python-get-s3-object/app.py
+++ lambda-python-get-s3-object/app.py
@@ -5,49 +5,68 @@ import pickle
 from sklearn import datasets
 from sklearn import svm
 
+# Instantiate S3 client
+s3 = boto3.client(service_name = 's3',
+                  endpoint_url = f"http://{os.getenv('LOCALSTACK_HOSTNAME')}:{os.getenv('EDGE_PORT')}")
+
+
 def handler(event, context):
     """Function to fetch object from S3"""
-    iris = datasets.load_iris()
 
-    X, y = iris.data, iris.target
+    # Loop over all records
+    for record in event['Records']:
+        # Load record body as JSON
+        record_body = json.loads(record['body'])
+        
+        # Get task and User ID
+        task = record_body["task"]
+        user_id = record_body["userID"]
 
-    # Instantiate S3 client
-    s3 = boto3.client(service_name = 's3',
-                      endpoint_url = f"http://{os.getenv('LOCALSTACK_HOSTNAME')}:{os.getenv('EDGE_PORT')}")
-    
-    # Instantiate SVC model
-    clf = svm.SVC()
+        # Check if task is to train a model
+        if(task == "train"):
+            print("Task is to train: ", task)
+        # Otherwise, check if task is to predict using model
+        elif(task == "predict"):
+            iris = datasets.load_iris()
 
-    # Train SVC on iris dataset
-    clf.fit(X, y)
+            X, y = iris.data, iris.target
+            
+            # Instantiate SVC model
+            clf = svm.SVC()
 
-    # Store SVC model as a pickle file
-    pickled_model = pickle.dumps(clf)
+            # Train SVC on iris dataset
+            clf.fit(X, y)
 
-    # Put pickled model to S3 bucket
-    s3.put_object(
-        Bucket = "sample-bucket",
-        Key = "pickled_model.pickle",
-        Body = pickled_model
-    )
-    
-    # Get object from S3 Bucket
-    bucket_object = s3.get_object(
-        Bucket = "sample-bucket",
-        Key = "pickled_model.pickle"
-    )
+            # Store SVC model as a pickle file
+            pickled_model = pickle.dumps(clf)
 
-    # Get object body
-    bucket_object_body = bucket_object['Body'].read()
+            # Put pickled model to S3 bucket
+            s3.put_object(
+                Bucket = "sample-bucket",
+                Key = "pickled_model_" + str(user_id) + ".pickle",
+                Body = pickled_model
+            )
+            
+            # Get object from S3 Bucket
+            bucket_object = s3.get_object(
+                Bucket = "sample-bucket",
+                Key = "pickled_model_" + str(user_id) + ".pickle"
+            )
 
-    # Get model from the body
-    ml_model = pickle.loads(bucket_object_body)
+            # Get object body
+            bucket_object_body = bucket_object['Body'].read()
 
-    # Perform prediction on an instance of dataset
-    ml_prediction = ml_model.predict(X[0:1])
+            # Get model from the body
+            ml_model = pickle.loads(bucket_object_body)
 
-    # Return from the function
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Prediction on ' + str(X[0:1]) + ' is : ' + str(ml_prediction))
-    }
+            # Perform prediction on an instance of dataset
+            ml_prediction = ml_model.predict(X[0:1])
+
+            # Print prediction to log
+            print("Prediction on " + str(X[0:1]) + " is : " + str(ml_prediction))
+
+            # Return from the function
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Prediction on ' + str(X[0:1]) + ' is : ' + str(ml_prediction))
+            }
