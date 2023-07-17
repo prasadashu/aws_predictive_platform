3a39ec64a05dd0eb35e8430ea2aa941609a7577d
Ashu Prasad <ashuprasad29516@gmail.com>
2023-07-17 17:26:09 -0500

    feat: added functionality to dynamically upload files

--- lambda-api-gateway-s3-integration/client_app.sh
+++ lambda-api-gateway-s3-integration/client_app.sh
@@ -3,7 +3,7 @@
 # Check if parameters were provided
 if [ $# -eq 0 ]; then
 	>&2 echo "Please specify the REST API ID and Deployment ID"
-	>&2 echo "Example: bash client_app.sh --restapiid=<SOME_REST_API_ID> --deployment=<SOME_DEPLOYMENT_ID>"
+	>&2 echo "Example: bash client_app.sh --restapiid=<SOME_REST_API_ID> --deployment=<SOME_DEPLOYMENT_ID> --userid=<SOME_USER_ID> --file=<SOME_LOCAL_FILE>"
 	exit 1
 fi
 
@@ -20,6 +20,10 @@ for i in "$@"; do
 			;;
 		-u=*|--userid=*)
 			USERID="${i#*=}"
+			shift
+			;;
+		-f=*|--file=*)
+			FILE="${i#*=}"
 			shift
 			;;
 		-*|--*)
@@ -40,4 +44,4 @@ echo "USER ID:  ${USERID}";
 url=$(curl -s -d "{\"userID\":\"${USERID}\"}" -H "Content-Type: application/json" -X POST "http://localhost:4566/restapis/${RESTAPIID}/${DEPLOYMENT}/_user_request_/pre-signed-s3-url" | jq '. | .preSignedUrl' | sed -e 's/^"//' -e 's/"$//')
 
 # Uplaod file using S3 pre-signed URL
-curl -s --upload-file sample_file.txt "$url"
+curl -s --upload-file "${FILE}" "$url"
