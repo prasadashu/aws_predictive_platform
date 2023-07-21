2cd1ca024b78d193659d5ef0309d37ebbbef9c68
Ashu Prasad <ashuprasad29516@gmail.com>
2023-07-21 00:22:29 -0500

    debug: exported env variables

--- scripts/setup_env_vars.sh
+++ scripts/setup_env_vars.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Defining Lambda function names
-PLATFORM_LAMBDA_FUNCTION="lambda-python-function"
-PREDICTIVE_LAMBDA_FUNCTION="lambda-platform-gateway-function"
+PLATFORM_LAMBDA_FUNCTION="lambda-platform-gateway-function"
+PREDICTIVE_LAMBDA_FUNCTION="lambda-python-function"
 
 # Defining REST API names
 PLATFORM_API_NAME="platform-gateway"
-PREDICTIVE_API_NAME="plredictive-gateway"
+PREDICTIVE_API_NAME="predictive-gateway"
 
 # Defining REST API deployment names
 PLATFORM_API_DEPLOYMENT_NAME="test"
 PREDICTIVE_API_DEPLOYMENT_NAME="test"
+
+# Export variables
+export PLATFORM_LAMBDA_FUNCTION
+export PREDICTIVE_LAMBDA_FUNCTION
+export PLATFORM_API_NAME
+export PREDICTIVE_API_NAME
+export PLATFORM_API_DEPLOYMENT_NAME
+export PREDICTIVE_API_DEPLOYMENT_NAME
