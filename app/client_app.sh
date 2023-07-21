aa8c6514d9eefc1288fe606903f1f722b84b3c9e
Ashu Prasad <ashuprasad29516@gmail.com>
2023-07-20 20:49:05 -0500

    refactor: abstracted REST API ID from user

--- app/client_app.sh
+++ app/client_app.sh
@@ -3,17 +3,13 @@
 # Check if parameters were provided
 if [ $# -eq 0 ]; then
 	>&2 echo "Please specify the REST API ID and Deployment ID"
-	>&2 echo "Example: bash client_app.sh --restapiid=<SOME_REST_API_ID> --deployment=<SOME_DEPLOYMENT_ID> --userid=<SOME_USER_ID> --file=<SOME_LOCAL_FILE>"
+	>&2 echo "Example: bash client_app.sh --deployment=<SOME_DEPLOYMENT_ID> --userid=<SOME_USER_ID> --file=<SOME_LOCAL_FILE>"
 	exit 1
 fi
 
 # Get REST API ID and Deployment name through CLI
 for i in "$@"; do
 	case $i in
-		-i=*|--restapiid=*)
-			RESTAPIID="${i#*=}"
-			shift
-			;;
 		-d=*|--deployment=*)
 			DEPLOYMENT="${i#*=}"
 			shift
@@ -36,12 +32,12 @@ for i in "$@"; do
 done
 
 # Print argument values
-echo "REST API ID: ${RESTAPIID}";
+echo "REST API ID: $PLATFORM_API_ID";
 echo "DEPLOYMENT Name: ${DEPLOYMENT}";
 echo "USER ID:  ${USERID}";
 
 # Get S3 pre-signed URL 
-url=$(curl -s -d "{\"userID\":\"${USERID}\"}" -H "Content-Type: application/json" -X POST "http://localhost:4566/restapis/${RESTAPIID}/${DEPLOYMENT}/_user_request_/pre-signed-s3-url" | jq '. | .preSignedUrl' | sed -e 's/^"//' -e 's/"$//')
+url=$(curl -s -d "{\"userID\":\"${USERID}\"}" -H "Content-Type: application/json" -X POST "http://localhost:4566/restapis/$PLATFORM_API_ID/${DEPLOYMENT}/_user_request_/pre-signed-s3-url" | jq '. | .preSignedUrl' | sed -e 's/^"//' -e 's/"$//')
 
 # Uplaod file using S3 pre-signed URL
 curl -s --upload-file "${FILE}" "$url"
